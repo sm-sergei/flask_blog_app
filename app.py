69f4361aaba09438236c10b5bfae11da097be33b
sm-sergei <sme-sergei>
2023-10-31 23:13:50 +0300

    Add write-blog logic and frontend for this

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, flash, session, request, redirect
 from flask_bootstrap import Bootstrap
 import yaml, psycopg2, os
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_ckeditor import CKEditor
 
 
 app = Flask(__name__)
 Bootstrap(app)
+CKEditor(app)
 
 app.config['SECRET_KEY'] = os.urandom(24)
 
@@ -22,7 +24,15 @@ def get_db_connection():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM blogs")
+    blogs = cur.fetchall()
+    if blogs:
+        cur.close()
+        conn.close()
+        return render_template('index.html', blogs=blogs)
+    return render_template('index.html', blogs=None)
 
 @app.route('/about/')
 def about():
@@ -31,7 +41,13 @@ def about():
 @app.route('/blogs/<int:id>')
 def blogs(id):
     """Функция возвращает кокретный блог по его id"""
-    return render_template('blogs.html', blog_id=id)
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM blogs WHERE blog_id=%s", (id,))
+    blog = cur.fetchone()
+    if blog:
+        return render_template('blogs.html', blog=blog)
+    return "<h1>Blog is not found</h1>"
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
@@ -79,12 +95,28 @@ def login():
             flash("User does not exist!", "danger")
             return render_template("login.html")
         cur.close()
+        conn.close()
         return redirect('/')
 
     return render_template('login.html')
 
 @app.route('/write-blog/', methods=['GET', 'POST'])
 def write_blog():
+    if request.method == "POST":
+        blogpost = request.form
+        title = blogpost['title']
+        body = blogpost['body']
+        author = session['first_name'] + " " + session['last_name']
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO blogs (title, author, body) VALUES (%s, %s, %s)", (title, author, body))
+        conn.commit()
+        cur.close()
+        conn.close()
+        flash("Your blog post is successfully posted!", "success")
+        return redirect('/')
+
+
     return render_template('write-blog.html')
 
 @app.route('/my-blogs/')
